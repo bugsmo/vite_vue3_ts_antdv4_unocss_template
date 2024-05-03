cf6830e39612407ab0d1032e56ec6adae6c7a994
bugsmo <kalandramo@gmail.com>
2024-05-03 23:53:25 +0800

    :building_construction: build: build: 打包配置优化

--- src/config/interceptors/router.ts
+++ src/config/interceptors/router.ts
@@ -1,6 +1,8 @@
 import { RouteLocationNormalized, NavigationGuardNext } from 'vue-router'
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function routerBeforeEachFunc(to: RouteLocationNormalized, from: RouteLocationNormalized, next: NavigationGuardNext) {
+  console.log(to, from)
   // 路由进入前的操作
   next()
 }
@@ -8,4 +10,5 @@ export async function routerBeforeEachFunc(to: RouteLocationNormalized, from: Ro
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function routerAfterEachFunc(to: RouteLocationNormalized, from: RouteLocationNormalized) {
   // 路由进入后的操作
+  console.log(to, from)
 }
